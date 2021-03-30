05204642bf81442dd46eef87b535b15d23ec12a5
Sattvik Palta <sattvikpalta@gmail.com>
2021-03-30 19:50:32 +0530

    Number system questions

---
+++ src/bas/funcnarrays/AnyToAny.java
@@ -0,0 +1,43 @@
+package bas.funcnarrays;
+
+import java.util.Scanner;
+
+public class AnyToAny {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sourceBase = scn.nextInt();
+        int destBase = scn.nextInt();
+
+        int decNum = anyToDec(n, sourceBase);
+        System.out.println(decToAny(decNum, destBase));
+    }
+
+    public static int anyToDec(int n, int sb) {
+        int rv = 0;
+        int sbPower = 1;
+
+        while (n != 0) {
+            int rem = n % 10;
+            rv += rem * sbPower;
+            sbPower *= sb;
+            n /= 10;
+        }
+
+        return rv;
+    }
+
+    public static int decToAny(int decNum, int db) {
+        int rv = 0;
+        int tenPower = 1;
+
+        while (decNum != 0) {
+            int rem = decNum % db;
+            rv += rem * tenPower;
+            tenPower *= 10;
+            decNum /= db;
+        }
+
+        return rv;
+    }
+}
--- src/bas/funcnarrays/AnyToDec.java
+++ src/bas/funcnarrays/AnyToDec.java
@@ -2,27 +2,26 @@ package bas.funcnarrays;
 
 import java.util.Scanner;
 
-public class DecToAny {
+public class AnyToDec {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int b = scn.nextInt();
-        int dn = getValueInBase(n, b);
-        System.out.println(dn);
+        int d = getValueIndecimal(n, b);
+        System.out.println(d);
     }
 
-    public static int getValueInBase(int n, int b) {
+    public static int getValueIndecimal(int n, int b) {
         int rv = 0;
-        int power = 0;
+        int bPower = 1;
 
         while (n != 0) {
-            int rem = n % b;
-            rv += rem * (int) Math.pow(10, power);
-            n /= b;
-            power++;
+            int rem = n % 10;
+            rv += rem * bPower;
+            bPower *= b;
+            n /= 10;
         }
 
         return rv;
     }
-
 }
