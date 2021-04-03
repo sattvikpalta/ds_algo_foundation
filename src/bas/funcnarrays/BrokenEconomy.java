915e712a4f1b83672e41722194ac7e0b365c27fa
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-03 17:38:46 +0530

    Functions and arrays complete

---
+++ src/bas/funcnarrays/BrokenEconomy.java
@@ -0,0 +1,42 @@
+package bas.funcnarrays;
+
+import java.util.Scanner;
+
+public class BrokenEconomy {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scn.nextInt();
+        }
+
+        int data = scn.nextInt();
+        scn.close();
+
+        int lo = 0;
+        int hi = a.length - 1;
+        int floor = Integer.MIN_VALUE;
+        int ceil = Integer.MAX_VALUE;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (data > a[mid]) {
+                lo = mid + 1;
+                floor = a[mid];
+            } else if (data < a[mid]) {
+                hi = mid - 1;
+                ceil = a[mid];
+            } else {
+                ceil = a[mid];
+                floor = a[mid];
+                break;
+            }
+        }
+
+        System.out.println(ceil);
+        System.out.println(floor);
+    }
+
+}
---
+++ src/bas/funcnarrays/FirstIdxLastIdx.java
@@ -0,0 +1,58 @@
+package bas.funcnarrays;
+
+import java.util.Scanner;
+
+public class FirstIdxLastIdx {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scn.nextInt();
+        }
+
+        int data = scn.nextInt();
+        scn.close();
+
+        int lo = 0;
+        int hi = a.length - 1;
+        int firstIdx = 0;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (data > a[mid]) {
+                lo = mid + 1;
+            } else if (data < a[mid]) {
+                hi = mid - 1;
+            } else {
+                firstIdx = mid;
+                hi = mid - 1;
+            }
+        }
+
+        System.out.println(firstIdx);
+
+        lo = 0;
+        hi = a.length - 1;
+        int lastIdx = 0;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (data > a[mid]) {
+                lo = mid + 1;
+            } else if (data < a[mid]) {
+                hi = mid - 1;
+            } else {
+                lastIdx = mid;
+                lo = mid + 1;
+            }
+        }
+
+        System.out.println(lastIdx);
+
+    }
+
+}
