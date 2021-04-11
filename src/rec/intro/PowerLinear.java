017f653676d8b7c9c4eb69e778d27a1c4b19904c
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-11 17:01:22 +0530

    Power of a number

---
+++ src/rec/intro/PowerLinear.java
@@ -0,0 +1,23 @@
+package rec.intro;
+
+import java.util.Scanner;
+
+public class PowerLinear {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        scn.close();
+
+        System.out.println(power(x, n));
+    }
+
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return x * power(x, n - 1);
+    }
+
+}
