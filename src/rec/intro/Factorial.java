1072069308c6a7e29f51e42d8dfd3035376bcf95
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-11 15:34:56 +0530

    Factorial of a number

---
+++ src/rec/intro/Factorial.java
@@ -0,0 +1,24 @@
+package rec.intro;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int fact = n;
+        fact *= factorial(n - 1);
+        return fact;
+    }
+
+}
