1c1feece0e145b2cc926ab418d985c20f92a6cbf
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-11 15:17:45 +0530

    Print increasing and decreasing numbers recursively

---
+++ src/bas/recursion/PrintDecreasing.java
@@ -0,0 +1,23 @@
+package bas.recursion;
+
+import java.util.Scanner;
+
+public class PrintDecreasing {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        printDecreasing(n);
+    }
+
+    public static void printDecreasing(int n) {
+        if (n == 0) {
+            return;
+        }
+        
+        System.out.println(n);
+        printDecreasing(n - 1);
+    }
+
+}
---
+++ src/bas/recursion/PrintIncreasing.java
@@ -0,0 +1,23 @@
+package bas.recursion;
+
+import java.util.Scanner;
+
+public class PrintIncreasing {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n) {
+        if (n == 0) {
+            return;
+        }
+        
+        printIncreasing(n - 1);
+        System.out.println(n);
+    }
+    
+}
