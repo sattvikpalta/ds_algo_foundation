1a2f24df555f46a968d91131010c1da8ab1349b6
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-11 13:58:47 +0530

    All permutations of a string

---
+++ src/bas/strings/StringPermutations.java
@@ -0,0 +1,46 @@
+package bas.strings;
+
+import java.util.Scanner;
+
+public class StringPermutations {
+    public static void solution(String str) {
+        int fact = factorial(str.length());
+
+        for (int i = 0; i < fact; i++) {
+            StringBuilder sb = new StringBuilder(str);
+            int temp = i;
+
+            for (int div = sb.length(); div > 0; div--) {
+                int q = temp / div;
+                int r = temp % div;
+
+                System.out.print(sb.charAt(r));
+                sb.deleteCharAt(r);
+
+                temp = q;
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+
+        for (int i = n; i > 1; i--) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        scn.close();
+
+        solution(str);
+    }
+
+}
