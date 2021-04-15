3cbadcdab9824cf58818152b72e140e1593b6c22
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-15 22:52:09 +0530

    Print numbers in zig zag

---
+++ src/rec/intro/ZigZag.java
@@ -0,0 +1,26 @@
+package rec.intro;
+
+import java.util.Scanner;
+
+public class ZigZag {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        pzz(n);
+    }
+
+    public static void pzz(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+    }
+
+}
