45d881bbcf6ca602b447cf3650127ff15dbb518b
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-19 20:18:25 +0530

    Print all maze paths using question-answer approach

---
+++ src/rec/recwayup/PrintMazePaths.java
@@ -0,0 +1,33 @@
+package rec.recwayup;
+
+import java.util.Scanner;
+
+public class PrintMazePaths {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        scn.close();
+
+        printMazePaths(1, 1, n, m, "");
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if (sr == dr && sc == dc) {
+            System.out.println(psf);
+            return;
+        }
+
+        if (sr > dr || sc > dc) {
+            return;
+        }
+
+        printMazePaths(sr, sc + 1, dr, dc, psf + 'h');
+        printMazePaths(sr + 1, sc, dr, dc, psf + 'v');
+    }
+
+}
