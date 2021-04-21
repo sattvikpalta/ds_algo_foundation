d55d2e9d02ded8fb5d28cf5b6e60a51553aa63ca
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-21 23:29:31 +0530

    Print total number of stair paths using memoisation and tabulation

--- src/dp/dpngreedy/ClimbStairs.java
+++ src/dp/dpngreedy/ClimbStairs.java
@@ -8,23 +8,47 @@ public class ClimbStairs {
         int n = scn.nextInt();
         scn.close();
 
+        System.out.println(countStairPaths(n, new int[n + 1]));
         System.out.println(countStairPaths(n));
+
     }
 
-    public static int countStairPaths(int n) {
+    // Memoisation
+    public static int countStairPaths(int n, int[] bank) {
         if (n == 0) {
             return 1;
         } else if (n < 0) {
             return 0;
         }
 
-        int nm1 = countStairPaths(n - 1);
-        int nm2 = countStairPaths(n - 2);
-        int nm3 = countStairPaths(n - 3);
+        if (bank[n] != 0) {
+            return bank[n];
+        }
+
+        int nm1 = countStairPaths(n - 1, bank);
+        int nm2 = countStairPaths(n - 2, bank);
+        int nm3 = countStairPaths(n - 3, bank);
 
         int count = nm1 + nm2 + nm3;
+        bank[n] = count;
 
         return count;
     }
 
+    // Tabulation
+    public static int countStairPaths(int n) {
+        int[] bank = new int[n + 1];
+        bank[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                bank[i] = bank[i - 1];
+            } else if (i == 2) {
+                bank[i] = bank[i - 1] + bank[i - 2];
+            } else {
+                bank[i] = bank[i - 1] + bank[i - 2] + bank[i - 3];
+            }
+        }
+
+        return bank[n];
+    }
 }
