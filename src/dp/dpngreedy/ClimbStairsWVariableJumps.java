ad8ee1d72965bd179174f7ef910d20f194c98734
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-23 00:43:22 +0530

    Climb stairs with variable jumpss

--- src/dp/dpngreedy/ClimbStairsWVariableJumps.java
+++ src/dp/dpngreedy/ClimbStairsWVariableJumps.java
@@ -13,40 +13,20 @@ public class ClimbStairsWVariableJumps {
 
         scn.close();
 
-        countStairPaths(n, 0, arr);
-
-    }
-
-    // arr[10] = {3 3 0 2 1 2 4 2 0 0}
-    public static void countStairPaths(int n, int count, int[] arr) {
-        if (n == 0) {
-            count += 1;
-            return;
-        } else if (n < 0) {
-            return;
-        }
-
-        if (n < arr.length) {
-            if (arr[n] == 0) {
-                return;
-            }
-        }
-
-        for (int i = 1; i <= arr[n - 1]; i++) {
-            countStairPaths(n - i, count, arr);
-        }
-
-        System.out.println(count + 1);
+        System.out.println(countStairPaths2(n, arr));
     }
 
     // Tabulation
-    /*
-     * public static int countStairPaths(int n, int[] arr) { int[] bank = new int[n
-     * + 1]; bank[n] = 1;
-     * 
-     * for (int i = n - 1; i >= 0; i--) { for (int j = 1; j <= arr[i] && i + j <
-     * bank.length; j++) { bank[i] += bank[i + j]; } }
-     * 
-     * return bank[0]; }
-     */
+    public static int countStairPaths2(int n, int[] arr) {
+        int[] bank = new int[n + 1];
+        bank[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= arr[i] && i + j < bank.length; j++) {
+                bank[i] += bank[i + j];
+            }
+        }
+
+        return bank[0];
+    }
 }
