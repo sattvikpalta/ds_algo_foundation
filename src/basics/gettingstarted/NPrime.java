45a926c799af3c22c12a5c303e3e168ae12c3e9a
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-24 13:43:17 +0530

    Print all prime numbers between 2 numbers

--- src/basics/gettingstarted/NPrime.java
+++ src/basics/gettingstarted/NPrime.java
@@ -2,8 +2,9 @@ package basics.gettingstarted;
 
 import java.util.Scanner;
 
-// Program to display prime numbers between 2 numbers
 public class NPrime {
+    // Program to display prime numbers between 2 numbers
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         // First number
@@ -12,19 +13,26 @@ public class NPrime {
         int hi = scn.nextInt();
         scn.close();
 
+        // Loop till lo <= hi
         while (lo <= hi) {
             int count = 0;
+            // Loop from 2 to square root of 'lo'
             for (int div = 2; div * div <= lo; div++) {
                 if (lo % div == 0) {
+                    // If a number divides 'lo' then increase count
                     count++;
+                    // If even one number divides 'lo' no need to check further, break
                     break;
                 }
             }
 
+            // Count = 0 indicates no number was able to divide 'lo', so print prime else
+            // print not prime
             if (count == 0) {
                 System.out.println(lo);
             }
 
+            // Increment 'lo' to check subsequent numbers
             lo++;
         }
     }
