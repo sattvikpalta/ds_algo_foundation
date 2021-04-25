630d9f6faf801b87ff7e82fac7c6117066d4493d
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-25 15:31:44 +0530

    Print reverse of a number

--- src/basics/gettingstarted/RevNum.java
+++ src/basics/gettingstarted/RevNum.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class RevNum {
     public static void main(String[] args) {
+        // Program to calculate reverse of a number
         Scanner scn = new Scanner(System.in);
+        // Input (Example: 1234)
         int n = scn.nextInt();
         scn.close();
 
         int rev = 0;
 
+        // Loop till number is not equal to zero
         while (n != 0) {
-            int rem = n % 10;
-            rev = rem + rev * 10;
-            n /= 10;
+            // Calculate remainder by dividing by 10
+            int rem = n % 10; // rem = 4
+
+            // Calculate reversed number by adding remainder and multiplying 'rev' by 10 for
+            // next place
+            rev = rem + rev * 10; // rev = 4
+            // Remove the remainder from 'n' by dividing 'n' by 10
+            n /= 10; // n = 123
         }
 
         System.out.println(rev);
