d239dc1b0dcff22a2801cf3e3cd0f1e8e330902c
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-25 16:40:54 +0530

    Check if given set of numbers is a valid pythagorean triplet

--- src/basics/gettingstarted/PyTriplet.java
+++ src/basics/gettingstarted/PyTriplet.java
@@ -3,16 +3,19 @@ package basics.gettingstarted;
 import java.util.Scanner;
 
 public class PyTriplet {
+    // Program to check if a given set of numbers is a valid pythagorean triplet
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int a = scn.nextInt();
-        int b = scn.nextInt();
-        int c = scn.nextInt();
+        // Input numbers
+        int a = scn.nextInt(); // 4
+        int b = scn.nextInt(); // 5
+        int c = scn.nextInt(); // 3
         scn.close();
 
-        if (a * a == b * b + c * c) {
+        if (a * a == b * b + c * c) { //
             System.out.println("true");
-        } else if (b * b == a * a + c * c) {
+        } else if (b * b == a * a + c * c) { // 5 x 5 = 4 x 4 + 3 x 3
             System.out.println("true");
         } else if (c * c == b * b + a * a) {
             System.out.println("true");
