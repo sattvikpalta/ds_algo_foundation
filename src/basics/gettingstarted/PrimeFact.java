3ddcd6c96ad732062d982364b2db6391b9df9dcc
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-25 16:28:56 +0530

    Print prime factorizations of a number

--- src/basics/gettingstarted/PrimeFact.java
+++ src/basics/gettingstarted/PrimeFact.java
@@ -3,17 +3,20 @@ package basics.gettingstarted;
 import java.util.Scanner;
 
 public class PrimeFact {
+    // Program to print prime factorizations of a number
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        // Input (Example: 56)
         int n = scn.nextInt();
         scn.close();
 
         int div = 2;
 
         while (n != 1) {
-            if (n % div == 0) {
-                System.out.print(div + " ");
-                n /= div;
+            if (n % div == 0) { // 56 % 2 = 0
+                System.out.print(div + " "); // Print 2
+                n /= div; // n = 28
                 continue;
             }
 
