3f608ab39364a547c1903d11bbb8a73dafadfecf
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-25 16:24:48 +0530

    Print all fibonacci numbers till n

--- src/basics/gettingstarted/NFibo.java
+++ src/basics/gettingstarted/NFibo.java
@@ -3,22 +3,25 @@ package basics.gettingstarted;
 import java.util.Scanner;
 
 public class NFibo {
-    public static void main(String[] args){
+    // Program to print all fibonacci numbers till n (using iteration)
+
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        // Input (Example: 5)
         int n = scn.nextInt();
         scn.close();
 
-        int first = 0;
-        int second = 1;
-        int i = 0;
+        int first = 0; // 1st fibonacci number is 0
+        int second = 1; // 2nd fibonacci number is 1
 
-        while(i < n){
+        int i = 0;
+        while (i < n) {
             System.out.println(first);
-            int third = first + second;
-            first = second;
-            second = third;
+            int third = first + second; // Next fibonacci is a sum of previous 2
+            first = second; // first = 1
+            second = third; // second = 1
             i++;
         }
     }
-    
+
 }
