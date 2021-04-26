0fe48df78ea7528467cfeebea255528e69c8c164
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:07:51 +0530

    Print mirror image of a right triangle using stars

--- src/basics/patterns/MirrTriangle.java
+++ src/basics/patterns/MirrTriangle.java
@@ -3,12 +3,14 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class MirrTriangle {
+    // Program to print mirror image of a right triangle using stars
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
         scn.close();
 
-        //Method - 1
+        // Method - 1
         /*
          * for (int row = 1; row <= numRows; row++) { for (int colSpace = row; colSpace
          * < numRows; colSpace++) { System.out.print("  "); }
@@ -18,9 +20,9 @@ public class MirrTriangle {
          * System.out.println(); }
          */
 
-         //Method - 2
-        int csp = numRows - 1;
-        int cst = 1;
+        // Method - 2
+        int csp = numRows - 1; // csp => count of spaces
+        int cst = 1; // cst => count of stars
 
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= csp; col++) {
@@ -31,8 +33,8 @@ public class MirrTriangle {
                 System.out.print("* ");
             }
 
-            csp--;
-            cst++;
+            csp--; // Decrease csp
+            cst++; // Increase cst
             System.out.println();
         }
     }
