632798d12d776b61ac27bf58f376300dea039df8
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-26 23:51:22 +0530

    Print an inverted right triangle using stars

--- src/basics/patterns/InvTriangle.java
+++ src/basics/patterns/InvTriangle.java
@@ -3,11 +3,14 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class InvTriangle {
+    // Program to print an inverted triangle using stars
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
         scn.close();
 
+        // Loop through rows where stars decrease as rows increase
         for (int row = 1; row <= numRows; row++) {
             for (int col = row; col <= numRows; col++) {
                 System.out.print("* ");
