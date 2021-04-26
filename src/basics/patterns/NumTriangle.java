7a8067ec145c398e131c96f7cb4e1d40c2ab58a2
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:35:42 +0530

    Print a right triangle of numbers

--- src/basics/patterns/NumTriangle.java
+++ src/basics/patterns/NumTriangle.java
@@ -3,11 +3,14 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class NumTriangle {
+    // Program to print a right triangle of numbers
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
         scn.close();
 
+        // Variable val to store number values
         int val = 1;
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= row; col++) {
