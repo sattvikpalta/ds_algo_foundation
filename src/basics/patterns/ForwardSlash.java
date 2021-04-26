6affca62492541b487975ce96fcdda316b04ae2c
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:25:25 +0530

    Print forward slash pattern

--- src/basics/patterns/ForwardSlash.java
+++ src/basics/patterns/ForwardSlash.java
@@ -3,6 +3,8 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class ForwardSlash {
+    // Program to print forward slash pattern
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
@@ -10,6 +12,7 @@ public class ForwardSlash {
 
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= numRows; col++) {
+                // Print '*' when row + col equals number of rows + 1 else print ' '
                 if (col + row == numRows + 1) {
                     System.out.print("* ");
                 } else {
