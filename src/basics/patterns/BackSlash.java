750617cf904b7b61a6f8224d6a664d42f86f7303
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:23:31 +0530

    Print back slash pattern

--- src/basics/patterns/BackSlash.java
+++ src/basics/patterns/BackSlash.java
@@ -3,6 +3,8 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class BackSlash {
+    // Program to print back slash pattern using stars
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
@@ -23,6 +25,7 @@ public class BackSlash {
         // Method - 2
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= numRows; col++) {
+                // Print '*' when row equals column else print ' '
                 if (row == col) {
                     System.out.print("* ");
                 } else {
