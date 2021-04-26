4e34174d5e59a4b7dfd8ef20ef2701651d5aee1d
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-26 23:35:27 +0530

    Print a right triangle using stars

--- src/basics/patterns/Triangle.java
+++ src/basics/patterns/Triangle.java
@@ -3,11 +3,14 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class Triangle {
+    // Program to print a right angle triangle using stars
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
         scn.close();
 
+        // Loop through rows where stars equal to row number
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
