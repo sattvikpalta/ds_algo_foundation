f28d20f8e3ddbda72b2e33484310ef71b54e9e89
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:59:54 +0530

    Print Swastik pattern

--- src/basics/patterns/Swastik.java
+++ src/basics/patterns/Swastik.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Swastik {
     // Program to print Swastik pattern
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
