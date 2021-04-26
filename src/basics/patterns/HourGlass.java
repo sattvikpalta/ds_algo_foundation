4da71c81755a032e8f746a583d512909e19fe2e6
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:57:37 +0530

    Print an hour glass pattern (top empty)

--- src/basics/patterns/HourGlass.java
+++ src/basics/patterns/HourGlass.java
@@ -3,6 +3,8 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class HourGlass {
+    // Program to print an hour glass pattern (top empty)
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
