675f30feea4faeab557493fde2a4ad2aece57ca2
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:54:35 +0530

    Print a half hollow diamond of numbers

--- src/basics/patterns/NumHalfHollowDiamond.java
+++ src/basics/patterns/NumHalfHollowDiamond.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class NumHalfHollowDiamond {
     // Program to print a half hollow diamond of numbers
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        // Input (Example: 5)
         int numRows = scn.nextInt();
         scn.close();
 
-        int csp = 2 * numRows - 3;
+        int csp = 2 * numRows - 3; // csp = 2 * 5 - 3 = 7
         int cst = 1;
 
         for (int row = 1; row <= numRows; row++) {
