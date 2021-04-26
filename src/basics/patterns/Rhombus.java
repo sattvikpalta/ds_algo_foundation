b9a24280e8529540c6bc33719f92df28268eea76
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:33:27 +0530

    Print rhombus pattern

--- src/basics/patterns/Rhombus.java
+++ src/basics/patterns/Rhombus.java
@@ -3,21 +3,27 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class Rhombus {
+    // Program to print a rhombus
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        // Input (Example: 5)
         int numRows = scn.nextInt();
         scn.close();
 
-        int outSpace = numRows / 2;
-        int inSpace = -1;
+        int outSpace = numRows / 2; // Outer spaces = 5 / 2 = 2
+        int inSpace = -1; // Inner spaces = -1
 
         for (int row = 1; row <= numRows; row++) {
+            // Loop to print outer spaces
             for (int col = 1; col <= outSpace; col++) {
                 System.out.print("  ");
             }
 
+            // Print a single '*' after outer spaces
             System.out.print("* ");
 
+            // Loop to print inner spaces
             for (int col = 1; col <= inSpace; col++) {
                 System.out.print("  ");
             }
