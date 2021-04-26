a642b9b91bbdbd6c393fb298d6808727e4f753df
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:28:18 +0530

    Print X pattern

--- src/basics/patterns/X.java
+++ src/basics/patterns/X.java
@@ -3,6 +3,8 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class X {
+    // Print 'X' pattern
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int numRows = scn.nextInt();
@@ -10,6 +12,8 @@ public class X {
 
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= numRows; col++) {
+                // Print '*' when row equals column or row + column equals number of rows + 1
+                // else print ' '
                 if (col == row || row + col == numRows + 1) {
                     System.out.print("* ");
                 } else {
