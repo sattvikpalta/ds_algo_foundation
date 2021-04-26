f08051c5d5d411a814e61369207aa832f285211c
Sattvik Palta <sattvikpalta@gmail.com>
2021-04-27 00:45:53 +0530

    Print multiplication table of a given number

--- src/basics/patterns/MulTable.java
+++ src/basics/patterns/MulTable.java
@@ -3,6 +3,8 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class MulTable {
+    // Program to print multiplication table of a given number
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
