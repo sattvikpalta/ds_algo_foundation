1e35f596305910dd9c96c8bbe86086d3137d1d8d
Sattvik Palta <sattvikpalta@gmail.com>
2021-05-03 20:37:03 +0530

    Convert a number from any given base to decimal base

--- src/basics/funcnarrays/AnyToDec.java
+++ src/basics/funcnarrays/AnyToDec.java
@@ -7,8 +7,8 @@ public class AnyToDec {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt(); // Input number (Example: 421)
-        int b = scn.nextInt(); //
+        int n = scn.nextInt(); // Input: number (Example: 1101)
+        int b = scn.nextInt(); // Input: source base (Example: 2)
         int d = getValueIndecimal(n, b);
         scn.close();
 
@@ -18,6 +18,14 @@ public class AnyToDec {
     public static int getValueIndecimal(int n, int b) {
         int rv = 0;
         int bPower = 1;
+
+        // Method to convert a number from a given base (i.e. binary) to decimal
+        // 2 ^ 0 x 1 = 1
+        // 2 ^ 1 x 0 = 0
+        // 2 ^ 2 x 1 = 4
+        // 2 ^ 3 x 1 = 8
+
+        // Return value = 1 + 0 + 4 + 8 = 13
 
         while (n != 0) {
             int rem = n % 10;
