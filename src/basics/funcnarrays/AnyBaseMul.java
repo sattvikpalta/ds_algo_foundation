de166f0eb6c8e8da663306ef0e164fa0de44a8ee
Sattvik Palta <sattvikpalta@gmail.com>
2021-05-07 16:56:30 +0530

    Multiply 2 numbers of a given base

--- src/basics/funcnarrays/AnyBaseMul.java
+++ src/basics/funcnarrays/AnyBaseMul.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class AnyBaseMul {
     public static void main(String[] args) {
+        // Program to multiply 2 numbers of a given base
 
         Scanner scn = new Scanner(System.in);
-        int b = scn.nextInt();
-        int n1 = scn.nextInt();
-        int n2 = scn.nextInt();
+        int b = scn.nextInt(); // Input: base value (Example: 10)
+        int n1 = scn.nextInt(); // Input: first number (Example: 542)
+        int n2 = scn.nextInt(); // Input: second number (Example: 51)
         scn.close();
 
+        // Call to getProduct function which returns product of n1 and n2
         int d = getProduct(b, n1, n2);
         System.out.println(d);
     }
@@ -20,9 +22,10 @@ public class AnyBaseMul {
         int power = 1;
 
         while (n2 != 0) {
-            int rem = n2 % 10;
-            n2 /= 10;
+            int rem = n2 % 10; // rem = 1
+            n2 /= 10; // n2 = 5
 
+            // Call to getSingleDigitProd which returns product of n1 and rem
             int singleProd = getSingleDigitProd(b, n1, rem);
             rv = getSum(b, rv, singleProd * power);
             power *= 10;
@@ -37,15 +40,15 @@ public class AnyBaseMul {
         int carry = 0;
 
         while (n1 != 0 || carry != 0) {
-            int r1 = n1 % 10;
-            n1 /= 10;
+            int r1 = n1 % 10; // r1 = 2
+            n1 /= 10; // n1 = 54
 
-            int digit = r1 * n2 + carry;
-            carry = digit / b;
-            digit = digit % b;
+            int digit = r1 * n2 + carry; // digit = 2 x 5 + 0 = 10
+            carry = digit / b; // 10 / 10 = 1
+            digit = digit % b; // 10 / 10 = 0
 
-            rv += digit * power;
-            power *= 10;
+            rv += digit * power; // rv = 10 x 1
+            power *= 10; // power = 10
         }
 
         return rv;
