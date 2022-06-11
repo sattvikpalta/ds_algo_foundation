7e10a7e48298c6cb696b95c88d79364974563703
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-11 14:57:02 -0400

    Rotate a number

--- src/basics/gettingstarted/RotNum.java
+++ src/basics/gettingstarted/RotNum.java
@@ -6,14 +6,35 @@ public class RotNum {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        // Input number (Example: 54321)
         int num = scn.nextInt();
-        // Input rotations (Example: 3)
-        int rot = scn.nextInt();
+        int numOfRotations = scn.nextInt();
         scn.close();
 
-        while (rot != 0) {
-            
+        int numOfDigits = 0;
+        int numCopy = num;
+        while (numCopy != 0) {
+            numCopy /= 10;
+            numOfDigits++;
         }
+
+        numOfRotations %= numOfDigits;
+        if (numOfRotations < 0){
+            numOfRotations += numOfDigits;
+        }
+
+        int divisor = 1;
+        int multiplier = 1;
+        for(int i = 1; i <= numOfDigits; i++) {
+            if(i <= numOfRotations) {
+                divisor *= 10;
+            } else {
+                multiplier *= 10;
+            }
+        }
+
+        int quotient = num / divisor;
+        int remainder = num % divisor;
+
+        System.out.println(remainder * multiplier + quotient);
     }
 }
