cfb2a7671916507cfdeeec1f6378374d8026296a
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-12 10:23:48 -0400

    Hollow diamond pattern

--- src/basics/patterns/HollowDiamond.java
+++ src/basics/patterns/HollowDiamond.java
@@ -3,45 +3,37 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class HollowDiamond {
-    // Program to print hollow diamond pattern
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        // Input (Example: 5)
         int numRows = scn.nextInt();
         scn.close();
 
-        int csp = 1;
-        int cst = numRows / 2 + 1; // cst = 5 / 2 + 1 = 3
+        int spaceCount = 1;
+        int starCount = numRows + 1;
 
         for (int row = 1; row <= numRows; row++) {
-            // Loop to print stars
-            for (int col = 1; col <= cst; col++) {
+            for (int col = 1; col <= starCount / 2; col++) {
                 System.out.print("* ");
             }
 
-            // Loop to print spaces
-            for (int col = 1; col <= csp; col++) {
+            for (int col = 1; col <= spaceCount; col++) {
                 System.out.print("  ");
             }
 
-            // Loop to print stars
-            for (int col = 1; col <= cst; col++) {
+            for (int col = 1; col <= starCount / 2; col++) {
                 System.out.print("* ");
             }
 
-            // For first half increase csp by 2 and decrease cst by 1
-            // For second half decrease csp by 1 and increase cst by 1
-            if (row <= numRows / 2) {
-                csp += 2;
-                cst--;
-            } else {
-                csp -= 2;
-                cst++;
-            }
-
             System.out.println();
+
+            if (row >= numRows / 2 + 1) {
+                spaceCount -= 2;
+                starCount += 2;
+            } else {
+                spaceCount += 2;
+                starCount -= 2;
+            }
         }
     }
-
 }
