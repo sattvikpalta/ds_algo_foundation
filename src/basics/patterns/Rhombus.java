ef0e6c67329803a58fe605b5aea2f9b5f8439329
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-12 12:03:58 -0400

    Rhombus pattern

--- src/basics/patterns/Rhombus.java
+++ src/basics/patterns/Rhombus.java
@@ -3,44 +3,38 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class Rhombus {
-    // Program to print a rhombus
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        // Input (Example: 5)
         int numRows = scn.nextInt();
         scn.close();
 
-        int outSpace = numRows / 2; // Outer spaces = 5 / 2 = 2
-        int inSpace = -1; // Inner spaces = -1
+        int outerSpaces = numRows / 2;
+        int innerSpaces = -1;
 
         for (int row = 1; row <= numRows; row++) {
-            // Loop to print outer spaces
-            for (int col = 1; col <= outSpace; col++) {
-                System.out.print("  ");
+            for (int col = 1; col <= outerSpaces; col++) {
+                System.out.print("\t");
             }
 
-            // Print a single '*' after outer spaces
-            System.out.print("* ");
-
-            // Loop to print inner spaces
-            for (int col = 1; col <= inSpace; col++) {
-                System.out.print("  ");
+            System.out.print("*\t");
+            for (int col = 1; col <= innerSpaces; col++) {
+                System.out.print("\t");
             }
 
-            if (row != 1 && row != numRows) {
-                System.out.print("* ");
-            }
-
-            if (row <= numRows / 2) {
-                outSpace--;
-                inSpace += 2;
-            } else {
-                outSpace++;
-                inSpace -= 2;
+            if (!(row == 1 || row == numRows)) {
+                System.out.print("*\t");
             }
 
             System.out.println();
+
+            if (row >= numRows / 2 + 1) {
+                outerSpaces++;
+                innerSpaces -= 2;
+            } else {
+                outerSpaces--;
+                innerSpaces += 2;
+            }
         }
     }
 }
