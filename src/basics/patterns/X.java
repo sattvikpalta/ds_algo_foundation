3f38a1b2856aa9350cbd4bf563672856aebf8776
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-12 11:03:24 -0400

    X shaped pattern

--- src/basics/patterns/X.java
+++ src/basics/patterns/X.java
@@ -3,7 +3,6 @@ package basics.patterns;
 import java.util.Scanner;
 
 public class X {
-    // Print 'X' pattern
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -12,17 +11,12 @@ public class X {
 
         for (int row = 1; row <= numRows; row++) {
             for (int col = 1; col <= numRows; col++) {
-                // Print '*' when row equals column or row + column equals number of rows + 1
-                // else print ' '
-                if (col == row || row + col == numRows + 1) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
+                if (col == row || (row + col) == numRows + 1)
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
             }
-
             System.out.println();
-        }
+        } 
     }
-
 }
