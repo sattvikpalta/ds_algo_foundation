6e1bae474ef8bbdd3ecaca9eaa2cedc291d3c879
Sattvik Palta <sattvikpalta@gmail.com>
2022-06-28 21:28:03 -0400

    Broken economy problem

--- src/basics/funcnarrays/BrokenEconomy.java
+++ src/basics/funcnarrays/BrokenEconomy.java
@@ -3,42 +3,40 @@ package basics.funcnarrays;
 import java.util.Scanner;
 
 public class BrokenEconomy {
-    // Pepcoding level 1 problem - Broken Economy
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int[] a = new int[n];
+        int[] arr = new int[n];
 
-        for (int i = 0; i < a.length; i++) {
-            a[i] = scn.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
         }
 
         int data = scn.nextInt();
         scn.close();
 
         int lo = 0;
-        int hi = a.length - 1;
-        int floor = Integer.MIN_VALUE;
-        int ceil = Integer.MAX_VALUE;
+        int hi = arr.length - 1;
+        int floor = 0;
+        int ceil = 0;
 
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
-            if (data > a[mid]) {
+            if (data > arr[mid]) {
                 lo = mid + 1;
-                floor = a[mid];
-            } else if (data < a[mid]) {
+                floor = arr[mid + 1];
+            } else if (data < arr[mid]) {
                 hi = mid - 1;
-                ceil = a[mid];
+                ceil = arr[mid - 1];
             } else {
-                ceil = a[mid];
-                floor = a[mid];
+                floor = arr[mid];
+                ceil = arr[mid];
                 break;
             }
         }
 
-        System.out.println(ceil);
         System.out.println(floor);
+        System.out.println(ceil);
     }
-
 }
