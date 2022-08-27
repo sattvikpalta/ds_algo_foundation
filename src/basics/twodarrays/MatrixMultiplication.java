b8f6c6874b0ef93b0767625c233167049af7fc83
Sattvik Palta <sattvikpalta@gmail.com>
2022-08-27 13:07:15 -0400

    Matrix multiplication

--- src/basics/twodarrays/MatrixMultiplication.java
+++ src/basics/twodarrays/MatrixMultiplication.java
@@ -3,51 +3,48 @@ package basics.twodarrays;
 import java.util.Scanner;
 
 public class MatrixMultiplication {
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int r1 = scn.nextInt();
-        int c1 = scn.nextInt();
-
-        int[][] one = new int[r1][c1];
-        for (int row = 0; row < one.length; row++) {
-            for (int col = 0; col < one[row].length; col++) {
-                one[row][col] = scn.nextInt();
+        int n1 = scn.nextInt();
+        int m1 = scn.nextInt();
+        int[][] a1 = new int[n1][m1];
+        for (int i = 0; i < a1.length; i++) {
+            for (int j = 0; j < a1[i].length; j++) {
+                a1[i][j] = scn.nextInt();
             }
         }
 
-        int r2 = scn.nextInt();
-        int c2 = scn.nextInt();
-
-        int[][] two = new int[r2][c2];
-        for (int row = 0; row < two.length; row++) {
-            for (int col = 0; col < two[row].length; col++) {
-                two[row][col] = scn.nextInt();
+        int n2 = scn.nextInt();
+        int m2 = scn.nextInt();
+        int[][] a2 = new int[n2][m2];
+        for (int i = 0; i < a2.length; i++) {
+            for (int j = 0; j < a2[i].length; j++) {
+                a2[i][j] = scn.nextInt();
             }
         }
 
         scn.close();
 
-        if (c1 != r2) {
+        if (m1 != n2) {
             System.out.println("Invalid input");
+            return;
         } else {
-            int[][] prod = new int[one.length][two[0].length];
-
-            for (int row = 0; row < prod.length; row++) {
-                for (int col = 0; col < prod[row].length; col++) {
-                    for (int k = 0; k < one[row].length; k++) {
-                        prod[row][col] += one[row][k] * two[k][col];
+            int[][] product = new int[n1][m2];
+            for (int i = 0; i < product.length; i++) {
+                for (int j = 0; j < product[i].length; j++) {
+                    for (int k = 0; k < m1; k++) {
+                        product[i][j] += a1[i][k] * a2[k][j];
                     }
                 }
             }
 
-            for (int row = 0; row < prod.length; row++) {
-                for (int col = 0; col < prod[row].length; col++) {
-                    System.out.print(prod[row][col] + " ");
+            for (int i = 0; i < product.length; i++) {
+                for (int j = 0; j < product[i].length; j++) {
+                    System.out.print(product[i][j] + " ");
                 }
-
                 System.out.println();
             }
         }
     }
-
 }
