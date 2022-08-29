ef92bb940ea9ecbdfbcc90b77140adf7c223af9b
Sattvik Palta <sattvikpalta@gmail.com>
2022-08-29 12:35:34 -0400

    Exit point in a matrix

--- src/basics/twodarrays/ExitPointMatrix.java
+++ src/basics/twodarrays/ExitPointMatrix.java
@@ -3,54 +3,54 @@ package basics.twodarrays;
 import java.util.Scanner;
 
 public class ExitPointMatrix {
+    public static void exitPoint(int[][] arr) {
+        int row = 0;
+        int col = 0;
+        int dir = 0;
+
+        while (true) {
+            if (row == arr.length || col == arr[0].length) {
+                if (row == arr.length) {
+                    System.out.println(row - 1);
+                    System.out.println(col);
+                    break;
+                } else {
+                    System.out.println(row);
+                    System.out.println(col - 1);
+                    break;
+                }
+            }
+
+            if (arr[row][col] != 0) {
+                dir++;
+                if (dir == 4)
+                    dir = 0;
+            }
+
+            if (dir == 0)
+                col++;
+            else if (dir == 1)
+                row++;
+            else if (dir == 2)
+                col--;
+            else if (dir == 3)
+                row--;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int m = scn.nextInt();
-        int[][] matrix = new int[n][m];
+        int rows = scn.nextInt();
+        int cols = scn.nextInt();
+        int[][] arr = new int[rows][cols];
 
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
-                matrix[row][col] = scn.nextInt();
-            }
-        }
+        for (int row = 0; row < rows; row++)
+            for (int col = 0; col < cols; col++)
+                arr[row][col] = scn.nextInt();
 
         scn.close();
 
-        int row = 0;
-        int col = 0;
-
-        int dir = 0; // 0 - east, 1 - south, 2 - west, 3 - north
-
-        while (true) {
-            dir = (dir + matrix[row][col]) % 4;
-            if (dir == 0) {
-                col++;
-            } else if (dir == 1) {
-                row++;
-            } else if (dir == 2) {
-                col--;
-            } else if (dir == 3) {
-                row--;
-            }
-
-            if (row == -1) {
-                row++;
-                break;
-            } else if (col == -1) {
-                col++;
-                break;
-            } else if (row == matrix.length) {
-                row--;
-                break;
-            } else if (col == matrix[0].length) {
-                col--;
-                break;
-            }
-        }
-
-        System.out.println(row);
-        System.out.println(col);
+        exitPoint(arr);
     }
 
 }
