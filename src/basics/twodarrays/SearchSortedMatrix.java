34c12c002e763275bfad3e5491e57f73781ac843
Sattvik Palta <sattvikpalta@gmail.com>
2022-09-01 11:18:17 -0400

    Search in a sorted matrix

--- src/basics/twodarrays/SearchSortedMatrix.java
+++ src/basics/twodarrays/SearchSortedMatrix.java
@@ -16,28 +16,17 @@ public class SearchSortedMatrix {
         int x = scn.nextInt();
         scn.close();
 
-        // Linear Search
-        /*
-         * for (int row = 0; row < matrix.length; row++) { for (int col = 0; col <
-         * matrix[row].length; col++) { if (matrix[row][col] == x) {
-         * System.out.println(row); System.out.println(col); return; } } }
-         * 
-         * System.out.println("Not Found");
-         */
-
-        // Binary Search
         int row = 0;
-        int col = matrix[0].length - 1;
-
+        int col = matrix.length - 1;
         while (row < matrix.length && col >= 0) {
             if (matrix[row][col] == x) {
                 System.out.println(row);
                 System.out.println(col);
                 return;
-            } else if (x < matrix[row][col]) {
-                col--;
-            } else {
+            } else if (matrix[row][col] < x) {
                 row++;
+            } else {
+                col--;
             }
         }
 
