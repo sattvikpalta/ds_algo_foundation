516853acd7d61cd8ea7d38397e3d1fd534e72688
Sattvik Palta <sattvikpalta@gmail.com>
2022-09-01 14:38:41 -0400

    Remove all primes from an array list

--- src/basics/strings/RemovePrimes.java
+++ src/basics/strings/RemovePrimes.java
@@ -4,37 +4,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RemovePrimes {
-    public static void solution(ArrayList<Integer> al) {
-        for (int i = al.size() - 1; i >= 0; i--) {
-            if(isPrime(al.get(i))){
-                al.remove(i);
-            }
-        }
-
-    }
-
-    public static boolean isPrime(int n) {
-        for (int div = 2; div * div <= n; div++) {
-            if (n % div == 0) {
+    public static boolean isPrime(int num) {
+        for (int div = 2; div * div <= num; div++)
+            if (num % div == 0)
                 return false;
-            }
-        }
 
         return true;
+    }
+
+    public static void removePrimes(ArrayList<Integer> list) {
+        for (int i = list.size() - 1; i >= 0; i--)
+            if (isPrime(list.get(i)))
+                list.remove(i);
     }
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        ArrayList<Integer> al = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            al.add(scn.nextInt());
-        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            list.add(scn.nextInt());
 
         scn.close();
 
-        solution(al);
-        System.out.println(al);
+        removePrimes(list);
+        System.out.println(list);
     }
 
 }
