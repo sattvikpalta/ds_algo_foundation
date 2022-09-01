d615f07c1e34447eb2e25363adaf64cb1324305f
Sattvik Palta <sattvikpalta@gmail.com>
2022-09-01 14:22:08 -0400

    Print all permutations of a string

--- src/basics/strings/StringPermutations.java
+++ src/basics/strings/StringPermutations.java
@@ -3,36 +3,30 @@ package basics.strings;
 import java.util.Scanner;
 
 public class StringPermutations {
-    public static void solution(String str) {
-        int fact = factorial(str.length());
+    public static int factorial(int num) {
+        int fact = num;
+        for (int i = num - 1; i > 0; i--)
+            fact *= i;
 
-        for (int i = 0; i < fact; i++) {
+        return fact;
+    }
+
+    public static void permutations(String str) {
+        int totalPermutations = factorial(str.length());
+
+        for (int i = 0; i < totalPermutations; i++) {
             StringBuilder sb = new StringBuilder(str);
             int temp = i;
 
-            for (int div = sb.length(); div > 0; div--) {
-                int q = temp / div;
-                int r = temp % div;
-
-                System.out.print(sb.charAt(r));
-                sb.deleteCharAt(r);
-
-                temp = q;
+            for (int div = str.length(); div > 0; div--) {
+                int rem = temp % div;
+                System.out.print(sb.charAt(rem));
+                sb.deleteCharAt(rem);
+                temp /= div;
             }
 
             System.out.println();
         }
-
-    }
-
-    public static int factorial(int n) {
-        int fact = 1;
-
-        for (int i = n; i > 1; i--) {
-            fact *= i;
-        }
-
-        return fact;
     }
 
     public static void main(String[] args) {
@@ -40,7 +34,7 @@ public class StringPermutations {
         String str = scn.next();
         scn.close();
 
-        solution(str);
+        permutations(str);
     }
 
 }
